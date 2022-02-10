6a851777101afe5478d19ba8836694a039090c8e
Anthony <tonyudeagbala@gmail.com>
2022-02-10 17:04:10 +0000

    Add condition for loop

--- news/tasks.py
+++ news/tasks.py
@@ -17,10 +17,12 @@ def published_news(self):
     response = requests.get(max_id_url)
     lower_bound = 1
     max_id = int(response.text)
-    for i in range(lower_bound, lower_bound + 10):
+    
+    if lower_bound < max_id:
+        for i in range(lower_bound, lower_bound + 10):
 
-        request_next_100_news = requests.get(base_url + f'item/{i}.json?print=pretty')
-        print(request_next_100_news.text)
-        lower_bound += 100
+            request_next_100_news = requests.get(base_url + f'item/{i}.json?print=pretty')
+            print(request_next_100_news.text)
+            lower_bound += 100
 
     return 'Done'
